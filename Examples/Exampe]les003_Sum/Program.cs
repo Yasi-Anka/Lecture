03b89240c2313012ae116c61ae668fb4b057f36a
Tatsiana <yasi-anka@bk.ru>
2022-10-16 20:46:37 +0300

    Сумма рандомных чисел

--- Examples/Exampe]les003_Sum/Program.cs
+++ Examples/Exampe]les003_Sum/Program.cs
@@ -3,4 +3,4 @@ Console.WriteLine(numberA);
 int numberB = new Random().Next(1, 10);
 Console.WriteLine(numberB);
 int result = numberA + numberB;
-Console.WriteLine(NonRandomizedStringEqualityComparer);
+Console.WriteLine(result);
