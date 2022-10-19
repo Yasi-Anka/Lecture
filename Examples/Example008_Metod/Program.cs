51a196e7269e5e5d670a8db1b2934b2e9148248f
Tatsiana <yasi-anka@bk.ru>
2022-10-19 12:46:13 +0300

    Переименование папок

--- Examples/Example008_Metod/Program.cs
+++ Examples/Example008_Metod/Program.cs
@@ -1,7 +1,7 @@
 ﻿// Найти максимальное из 9-ти чисел
 
 //Программа без функции/ "Стихийное" решение
-/*
+
 int a1 = 15;
 int b1 = 21;
 int c1 = 39;
@@ -25,7 +25,7 @@ if(b3 > max) max = b3;
 if(c3 > max) max = c3;
 
 Console.WriteLine(max);
-*/
+
 
 // Решение с использованием функции
 int Max(int arg1, int arg2, int arg3)
@@ -46,9 +46,14 @@ int a3 = 13;
 int b3 = 23;
 int c3 = 33;
 
-int max1 = Max(a1, b1, c1);
-int max2 = Max(a2, b2, c2);
-int max3 = Max(a3, b3, c3);
-int max = Max(max1, max2, max3);
+// int max1 = Max(a1, b1, c1);
+// int max2 = Max(a2, b2, c2);
+// int max3 = Max(a3, b3, c3);
+// int max = Max(max1, max2, max3);
+
+int max = Max(
+    Max(a1, b1, c1),
+    Max(a2, b2, c2),
+    Max(a3, b3, c3));
 
 Console.WriteLine(max);
